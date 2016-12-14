cb7d25c594e4c1725b74489d6b97c1c3c1f49aae
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-14 20:39:58 +0900

    イベント処理完了

--- src/H223.java
+++ src/H223.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class H223 extends JPanel {
-    JLabel label1, label2,  plus, minus, times, divid, surplus;
-    JTextField from, to;
+public class H223 extends JPanel implements KeyListener {
+    private JLabel plus, minus, times, divid, surplus;
+    private JTextField from, to;
 
     private H223(){
-        label1 = new JLabel("２つの整数をいれなさい");
-        label2 = new JLabel("データを入れるたびにENTERを押してください");
+        JLabel label1 = new JLabel("２つの整数をいれなさい");
+        JLabel label2 = new JLabel("データを入れるたびにENTERを押してください");
         label1.setFont(new Font("MS Gothic", Font.PLAIN, 20));
         label2.setFont(new Font("MS Gothic", Font.PLAIN, 20));
         from = new JTextField("200");
@@ -27,7 +29,24 @@ public class H223 extends JPanel {
         panel2.add(times); panel2.add(divid); panel2.add(surplus);
         setLayout(new GridLayout(0, 1));
         add(label1); add(label2); add(panel0); add(panel1); add(panel2);
+        from.addKeyListener(this);
+        to.addKeyListener(this);
     }
+
+    public void keyTyped(KeyEvent e) {}
+    public void keyPressed(KeyEvent e) {
+        int keycode = e.getKeyCode();
+        if (keycode == KeyEvent.VK_ENTER) {
+            int x = Integer.parseInt(from.getText());
+            int y = Integer.parseInt(to.getText());
+            plus.setText("加算 " + (x + y));
+            minus.setText("減算 " + (x - y));
+            times.setText("乗算 " + (x * y));
+            divid.setText("除算 " + (x / y));
+            surplus.setText("剰余 " + (x % y));
+        }
+    }
+    public void keyReleased(KeyEvent e) {}
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello");
